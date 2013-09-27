1ea4a052ea9d45183fd13fbb23e2e5498a1bc4b1
Isaac Rosenberg <irosenb7@gmail.com>
2013-09-27 19:41:05 -0400

    initial commit on rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cheerup::Application.config.secret_key_base = 'ceb7a69c6b3f64c6fac55a354a446075ac1dfc498e089cd57c0437c2f7f4e5bac267ea54bd3c2b72169d89c6b452807bd92374e54b201d57d6fa2d55c6c604e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cheerup::Application.config.session_store :cookie_store, key: '_cheerup_session'
