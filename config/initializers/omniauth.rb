8d4fb60b6a2e022d043d3605fba4d277223cc70b
John Kelly Ferguson <hello@johnkellyferguson.com>
2013-09-28 16:24:56 -0400

    Add moves tracking.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :jawbone,
   ENV['JAWBONE_CLIENT_ID'],
   ENV['JAWBONE_CLIENT_SECRET'],
-  scope: "basic_read mood_read sleep_read"
+  scope: "basic_read mood_read sleep_read move_read"
 end
