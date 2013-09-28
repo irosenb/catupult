429c8dff4e1f6aea8c8ae20f81bc47b446b4cff3
Isaac Rosenberg <irosenb7@gmail.com>
2013-09-28 17:32:56 -0400

    switching to new calendar branch

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,7 +1,5 @@
 class PublicController < ApplicationController
   def home
-  	if current_user 
-  		current_user.client
-  	end
+  	
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
 		@jawbone_client ||= Jawbone::Client.new(token)
 	end
 
+	
+
 	def self.find_or_create_from_jawbone(auth)
 		if user = where(uid: auth['uid']).first
 			user.token = auth['credentials']['token']
