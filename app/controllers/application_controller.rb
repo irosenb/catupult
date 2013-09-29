185efce671f8dca7a0472fb5952f3787a846c95d
Isaac Rosenberg <irosenb7@gmail.com>
2013-09-29 16:50:36 -0400

    jlasdf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   	puts params.inspect
   	events_hash = JSON.parse(params["events"])
     user = User.find(uid: events_hash['user_xid'])
-  	events_hash["events"].each do |event|
+  	events_hash.each do |event|
   		if event["event_type"] == "mood" and event["action"] == "creation"
   			sleep_event = event["event_xid"]
   		end
