a066cd7e722c1567d9c8be372d1df2f14fb4d835
Isaac Rosenberg <irosenb7@gmail.com>
2013-10-01 13:05:50 -0400

    simplifying code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	:before_save correct_phone_number
 
 	def has_phone_number?
 		phone_number != nil
@@ -18,22 +19,21 @@ class User < ActiveRecord::Base
 		jawbone_client.mood
 	end
 
+	def message(body)
+		message = twilio_client.account.sms.messages.create(
+				:body => "#{body}",
+		    :to   => "+1#{phone_number}",
+		    :from => "+18484562816")
+		puts message.sid
+	end
+
 	def get_mood
 		if mood["data"]["title"] == "Totally Done" or mood["data"]["title"] == "Dragging"
 			self.send_text
 		end
 	end
 
-	def tired_detector
-	end
-
 	def correct_phone_number
-			if phone_number.start_with?("1")
-				self.phone_number = phone_number.prepend("+")
-			elsif phone_number.start_with?("+1")
-			else
-				self.phone_number = phone_number.prepend("+1")
-			end
 			phone_number.gsub('-', '')
 			phone_number.gsub('(', '')
 			phone_number.gsub(')', '')
@@ -42,25 +42,13 @@ class User < ActiveRecord::Base
 	end
 
 	def recieve_options
-		twilio_client.account.messages.each do |message|
-    	puts message.body
+		twilio_client.account.list.each do |message|
+    	puts message.inspect
     end
 	end
 
 	def doggies
-		message = twilio_client.account.sms.messages.create(
-				:body => "Welcome to Catupult! Do you like dogs or cats? Text dogs to see dogs, or cats to see cats.",
-		    :to => "#{correct_phone_number}",     # Replace with your phone number
-		    :from => "+18484562816")   # Replace with your Twilio number
-		puts message.sid
-	end
-
-	def send_text
-		message = twilio_client.account.sms.messages.create(
-				:body => "Hey, you seem a little down. Here's a cat pic to cheer you up. http://placekitten.com/#{rand(400..600)}/#{rand(400..600)}",
-		    :to => "#{correct_phone_number}",     # Replace with your phone number
-		    :from => "+18484562816")   # Replace with your Twilio number
-		puts message.sid
+		send_message("Welcome to Catupult! Do you like dogs or cats? Text dogs to see dogs, or cats to see cats.")
 	end
 
 	def self.find_or_create_from_jawbone(auth)
@@ -75,10 +63,10 @@ class User < ActiveRecord::Base
 
 	def self.create_from_jawbone(auth)
 		create! do |user|
-			user.uid = auth['uid']
-			user.token = auth['credentials']['token']
+			user.uid        = auth['uid']
+			user.token      = auth['credentials']['token']
 			user.first_name = auth['info']['first_name']
-			user.last_name = auth['info']['last_name']
+			user.last_name  = auth['info']['last_name']
 		end
 	end
 end
